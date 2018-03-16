23d386793f3d1b982303181d33b5f90919319853
Aki.SHIMO <fezrestia@gmail.com>
2018-03-16 12:41:35 +0900

    ADD: Add blackscreen app

---
+++ app/src/main/java/com/demo/pet/petapp/BlackScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.demo.pet.petapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class BlackScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.black_screen)
+    }
+}
