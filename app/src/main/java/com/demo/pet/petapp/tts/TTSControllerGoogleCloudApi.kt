6a80c79fcd813a3a6d2825cf165f5c38cc9f0f26
Aki.SHIMO <fezrestia@gmail.com>
2019-08-24 13:51:28 +0900

    FIX: Change TTS-GCP AudioTrack usage to MEDIA to use out speaker

--- app/src/main/java/com/demo/pet/petapp/tts/TTSControllerGoogleCloudApi.kt
+++ app/src/main/java/com/demo/pet/petapp/tts/TTSControllerGoogleCloudApi.kt
@@ -194,7 +194,7 @@ class TTSControllerGoogleCloudApi(var context: Context?, val option: String) : T
 
             val p = AudioTrack.Builder()
                     .setAudioAttributes(AudioAttributes.Builder()
-                            .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
                             .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
                             .build())
                     .setAudioFormat(AudioFormat.Builder()
